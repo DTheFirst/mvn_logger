360cec6c2595c0cf81ae8d6486d01b4ef7a44853
DTheFirst <dimaleon82@gmail.com>
2022-04-18 14:57:29 +0200

    first commit

---
+++ src/main/java/by/vstu/febu/App.java
@@ -0,0 +1,13 @@
+package by.vstu.febu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
